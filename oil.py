7b810b2408fab51295486b3b949c078c95d0ce1a
mbm808 <mbmiller808@gmail.com>
2026-01-10 14:11:27 -0600

    i dont care

---
+++ oil.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import geopandas as gpd
+import rasterio as rio
+import matplotlib.pyplot as plt
+
+raw = pd.read_csv('C:\\Users\\mbmil\\Desktop\\KS oil.csv')
+raw = raw[raw["Status"] == "Producing"]
+usa = gpd.read_file("C:\\Users\\mbmil\\Desktop\\usa.zip")
+ks = usa[usa["STUSPS"] == "KS"]
+ax = ks.plot(edgecolor='black', facecolor='none')
+ax.set_axis_off()
+plt.scatter(raw["Longitude (NAD27)"], raw["Latitude (NAD27)"], s=0.25, c=raw["Total Depth"], cmap="rainbow")
+plt.colorbar(label="Well Depth (ft)")
+plt.show()
+
+
+# plt.hist(raw["Total Depth"], bins=100)
+# plt.show()
